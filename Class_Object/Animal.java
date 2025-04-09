8897e59baa4ef2711609f954b6104738c4e410bc
yeshaxds <132030326+yeshaxds@users.noreply.github.com>
2025-04-09 21:08:30 +0800

    Update Animal.java

--- Class_Object/Animal.java
+++ Class_Object/Animal.java
@@ -1,9 +1,16 @@
 package Class_Object;
-
-public class Animal {
+// 抽象类: 不能实例化, 用于继承
+public abstract class Animal {
     protected String name; // 封装: 外部无法直接访问
 
     public Animal(String name){
         this.name = name;
     }
+
+    public String getName(){
+        return name;
+    }
+
+    public abstract void eat();
+    public abstract void makeSound();
 }
