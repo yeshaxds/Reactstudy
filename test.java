62d04f8b8ee53ebcdbccd1efbb8c33137adcd931
yeshaxds <132030326+yeshaxds@users.noreply.github.com>
2025-04-09 20:39:20 +0800

    Update test.java

--- test.java
+++ test.java
@@ -1,7 +1,14 @@
-
+//# 所有的子类都必须从这个函数作为出口输出，而且一个文件夹中只能有一个main函数。
+import java.util.Scanner;
 public class test {
     public static void main(String[] args){
-        HelloWorld helloWorld = new HelloWorld();
-        helloWorld.show();
+        // 调用HelloWorld类
+        HelloWorld hw = new HelloWorld();//这种调用叫"对象"
+        hw.show();
+
+        // 枚举类调用
+        meiju m_j = new meiju();
+        m_j.size = meiju.Color.RED;
+        System.out.println(m_j.size);
     }
 }
