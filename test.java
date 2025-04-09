7df3c7907ac691fbe12e09cd1f41d31a0ec94c8f
yeshaxds <132030326+yeshaxds@users.noreply.github.com>
2025-04-09 21:08:20 +0800

    Update test.java

--- test.java
+++ test.java
@@ -1,4 +1,10 @@
 //# 所有的子类都必须从这个函数作为出口输出，而且一个文件夹中只能有一个main函数。
+// 不在一个文件夹下的子类需要在文件开头加上package 包名;HelloWord类和meiju枚举类都在同一个包下所以不需要调用包名。
+import Class_Object.Animal;
+import Class_Object.Bird;
+import Class_Object.Dog;
+import Class_Object.Flyable;
+
 import java.util.Scanner;
 public class test {
     public static void main(String[] args){
@@ -10,5 +16,14 @@ public class test {
         meiju m_j = new meiju();
         m_j.size = meiju.Color.RED;
         System.out.println(m_j.size);
+
+        // 类和对象的练习
+        Animal dog = new Dog("旺财");
+        Animal bird = new Bird("小鸟");
+        dog.eat();
+        bird.makeSound();
+
+        //向下转型调用接口方法
+        ((Flyable) bird).fly();
     }
 }
