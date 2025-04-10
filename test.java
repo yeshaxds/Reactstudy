6c081004efa6c5f2c9951f379590deef8cb7985b
yeshaxds <132030326+yeshaxds@users.noreply.github.com>
2025-04-10 11:51:23 +0800

    Update test.java

--- test.java
+++ test.java
@@ -1,3 +1,4 @@
+
 //# 所有的子类都必须从这个函数作为出口输出，而且一个文件夹中只能有一个main函数。
 // 不在一个文件夹下的子类需要在文件开头加上package 包名;HelloWord类和meiju枚举类都在同一个包下所以不需要调用包名。
 import Class_Object.Animal;
@@ -5,13 +6,15 @@ import Class_Object.Bird;
 import Class_Object.Dog;
 import Class_Object.Flyable;
 import Data_Type.*;
+import Xun_Huan.*;
 import java.util.Scanner;
+
 //一个源文件只能又一个public类，如果有多个public类，编译器会报错。
 // 包名和类名要一致，否则编译不通过。
 public class test {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         // 调用HelloWorld类
-        HelloWorld hw = new HelloWorld();//这种调用叫"对象"
+        HelloWorld hw = new HelloWorld();// 这种调用叫"对象"
         hw.show();
 
         // 枚举类调用
@@ -26,11 +29,18 @@ public class test {
         dog.eat();
         bird.makeSound();
 
-        //向下转型调用接口方法
+        // 向下转型调用接口方法
         ((Flyable) bird).fly();
 
         // 数据类型类
         Data_t data = new Data_t();
         data.show();
+
+        // 循环类
+        xun_H xun_h = new xun_H();
+        xun_h.for_XH();
+        xun_h.While_XH();
+        xun_h.DoWhile_XH();
+        xun_h.Switch_XH();
     }
 }
