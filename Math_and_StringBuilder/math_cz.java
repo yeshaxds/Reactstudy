c354e702829c0480f85006915ce49b63a1c70a0d
yeshaxds <132030326+yeshaxds@users.noreply.github.com>
2025-04-16 15:11:58 +0800

    Create math_cz.java

---
+++ Math_and_StringBuilder/math_cz.java
@@ -0,0 +1,16 @@
+package Math_and_StringBuilder;
+
+public class math_cz {
+    Integer a=10;
+    public double xxxV() {
+        return a.doubleValue();
+    }
+
+    public boolean Equals(String c, String b)
+    {
+        return c.equals(b);
+    }
+    public String toString(String name, int age) {
+        return "name: " + name + " age: " + age;
+    }
+}
