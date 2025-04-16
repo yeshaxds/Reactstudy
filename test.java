a31b4c21ca2ee90c180de1c366ae63911fb02279
yeshaxds <132030326+yeshaxds@users.noreply.github.com>
2025-04-16 15:11:42 +0800

    Update test.java

--- test.java
+++ test.java
@@ -7,40 +7,60 @@ import Class_Object.Dog;
 import Class_Object.Flyable;
 import Data_Type.*;
 import Xun_Huan.*;
+import Math_and_StringBuilder.*;
+import Data_Type.*;
 import java.util.Scanner;
 
 //一个源文件只能又一个public类，如果有多个public类，编译器会报错。
 // 包名和类名要一致，否则编译不通过。
 public class test {
     public static void main(String[] args) {
-        // 调用HelloWorld类
-        HelloWorld hw = new HelloWorld();// 这种调用叫"对象"
-        hw.show();
+//        // 调用HelloWorld类
+//        HelloWorld hw = new HelloWorld();// 这种调用叫"对象"
+//        hw.show();
+//
+//        // 枚举类调用
+//        meiju m_j = new meiju();
+//        m_j.size = meiju.Color.RED;
+//        System.out.println(m_j.size);
+//
+//        // 类和对象的练习
+//        // 父类调用子类方法
+//        Animal dog = new Dog("旺财");
+//        Animal bird = new Bird("小鸟");
+//        dog.eat();
+//        bird.makeSound();
+//
+//        // 向下转型调用接口方法
+//        ((Flyable) bird).fly();
+//
+//        // 数据类型类
+//        Data_t data = new Data_t();
+//        data.show();
+//        data.now_time();
+//
+//        // 循环类
+//        xun_H xun_h = new xun_H();
+//        xun_h.for_XH();
+//        xun_h.While_XH();
+//        xun_h.DoWhile_XH();
+//        xun_h.Switch_XH();
+//
+//        // Math方法调用
+//        math_cz math = new math_cz();
+//        System.out.println(math.xxxV());
+//        System.out.println(math.Equals("abc", "abc"));
+//        System.out.println(math.toString("小明", 18));
+//
+//        // StringBuilder and StringBuffer方法调用
+//        RunoobTest runoob = new RunoobTest();
+//        runoob.test();
+//        runoob.StringBufferTest();
 
-        // 枚举类调用
-        meiju m_j = new meiju();
-        m_j.size = meiju.Color.RED;
-        System.out.println(m_j.size);
+        // 动态数组的创建和使用
+        arr_cz arr = new arr_cz();
+        arr.arr_cs();
 
-        // 类和对象的练习
-        // 父类调用子类方法
-        Animal dog = new Dog("旺财");
-        Animal bird = new Bird("小鸟");
-        dog.eat();
-        bird.makeSound();
-
-        // 向下转型调用接口方法
-        ((Flyable) bird).fly();
-
-        // 数据类型类
-        Data_t data = new Data_t();
-        data.show();
-
-        // 循环类
-        xun_H xun_h = new xun_H();
-        xun_h.for_XH();
-        xun_h.While_XH();
-        xun_h.DoWhile_XH();
-        xun_h.Switch_XH();
+        //IO流的使用
     }
 }
